a96f514ca7e78d3a39679509306fa6fe05a30641
pikokr <parangee9706@gmail.com>
2020-12-02 22:55:42 +0900

    feat: noelle stats

--- migrate.sql
+++ migrate.sql
@@ -29,7 +29,7 @@ insert into characterMap (id, name, star, damage, brk, defence, maxhp, weapon)
 values ('kaeya', '케이아', 4, 41, 0, 57, 912, 'os'),
        ('xingqiu', '행추', 4, 41, 0, 57, 912, 'os'),
        ('razer', '레이저', 4, 41, 0, 57, 912, 'ts'),
-       ('noelle', '노엘', 4),
+       ('noelle', '노엘', 4, 57, 0, 57, 912, 'ts'),
        ('beidou', '북두', 4),
        ('chongyun', '중운', 4),
        ('amber', 'amber', 4),
