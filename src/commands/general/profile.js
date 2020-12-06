d02f183ef951267c0201c29b1978a90df6e0fb46
pikokr <parangee9706@gmail.com>
2020-12-06 12:25:48 +0900

    feat: alert if not registered

--- src/commands/general/profile.js
+++ src/commands/general/profile.js
@@ -17,7 +17,18 @@ module.exports = class extends Command {
     async exec(msg, {user: {id}}) {
         const u = await this.client.db('users').where({id}).then(r=>r[0])
         if (!u) {
-            return msg.reply('유저를 찾을 수 없습니다.')
+            if (msg.author.id === id) {
+                await msg.channel.send('프로필이 등록되지 않았습니다. DM을 확인해주세요.')
+                await msg.author.send(`"${this.client.commandHandler.prefix}계정" 명령어로 프로필을 등록하세요.`)
+            } else {
+                await msg.channel.send('해당 유저는 페이몬 서비스에 가입하지 않았습니다.')
+            }
+
+
+            return
         }
+        const Name = u.name, Money = u.money, Gem = u.gem, Molv = u.Mlv, CharacterLvl = u.Clv, Damage = u.damage, Defence = u.defence, NowHp = u.nowhp, Maxhp = u.maxhp
+        const res = await this.client.db('characters').where({id, have:true})
+        console.log(res)
     }
 }
--- type.d.ts
+++ type.d.ts
@@ -1,7 +1,10 @@
 declare module 'discord.js' {
     import Knex = require("knex");
+    import {CommandHandler, ListenerHandler} from "discord-akairo";
 
     interface Client {
         db: Knex
+        commandHandler: CommandHandler
+        listenerHandler: ListenerHandler
     }
 }
